389f22d37dfbe5d051e8f5812c9877269d284b3d
ravi6677 <ravikumar.alam@gmail.com>
2022-08-01 09:30:14 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,22 @@
-var trex, trex_running, trex_collided;
-var ground, groundImage, invisibleGround;
 
-function preload(){
-  
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadAnimation("trex_collided.png");
-  
-  groundImage = loadImage("ground2.png");
-  
-}
+import * as React from 'react';
+import {TextInput, StyleSheet, View} from 'react-native';
 
-function setup(){
-  
-  createCanvas(400,400);
-  
-  trex = createSprite(200,360,40,10);
-  trex.addAnimation("running",trex_running);
-  trex.scale = 0.5;
-  
-  ground = createSprite(200,380,400,20);
-  ground.addImage(groundImage);
-  
-  invisibleGround = createSprite(200,385,400,5);
-  invisibleGround.visible = false;
-  
-}
-
-function draw(){
-  
-  background("white");
-  
-  
-  //console.log(trex.y);
-  
-  trex.collide(invisibleGround);
-  
-  if(keyDown("space")&& trex.y >= 327){
-    trex.velocityY = -12;
+export default class App extends React.Component{
+  render(){
+    return(
+     <TextInput style = {styles.textContainer}/>
+     
+    )
   }
-  
-  trex.velocityY = trex.velocityY + 0.8;  
-  
-  ground.velocityX = -6;
-  
-  if(ground.x < 0){
-    ground.x = ground.width/2;
-  }
-  
-  
-  drawSprites();
-  
 }
+
+const styles = StyleSheet.create({
+  textContainer: {
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: "black",
+    width: 50,
+    height: 50,
+  }
+})
